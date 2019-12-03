6d08ad120c79123d729d694f781c6b7378a747db
gafrt8 <g.fijalkowski88@gmail.com>
2019-12-03 23:26:48 +0100

    Dodanie biblitek boost do testów i logów

--- src/TrueLife/common.h
+++ src/TrueLife/common.h
@@ -3,4 +3,20 @@
 
 #include <QDebug> // qDebug()<<"Hello!";
 
+#include <boost/log/trivial.hpp>
+/* [lecture 8, slide 31]
+ * BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+ * BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+ * BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+ * BOOST_LOG_TRIVIAL(error) << "An error severity message";
+ */
+
+#include <boost/test/test_tools.hpp>
+/* [lecture 8]
+ * BOOST_WARN / BOOST_CHECK / BOOST_REQUIRE     | slide 15
+ * _BITWISE_EQUAL / _EQUAL / _SMALL / _CLOSE    | slide 16
+ * _NO_THROW / _THROW / _EXCEPTION              | slide 17
+ * _MESSAGE / BOOST_ERROR / BOOST_FAIL          | slide 18
+ */
+
 #endif // COMMON_H
