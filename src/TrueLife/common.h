cb728b0cff668b104bdb0f5e06abf0a2df45ae63
gafrt8 <g.fijalkowski88@gmail.com>
2019-12-21 22:56:06 +0100

    Działający test testowania boost

--- src/TrueLife/common.h
+++ src/TrueLife/common.h
@@ -12,18 +12,6 @@
  * BOOST_LOG_TRIVIAL(error) << "An error severity message";
  */
 
-//#define BOOST_TEST_MODULE test module name
-//#define BOOST_TEST_DYN_LINK
-//#include <boost/test/test_tools.hpp> // lecture
-//#include <boost/test/unit_test.hpp> // https://www.boost.org/doc/libs/1_66_0/libs/test/doc/html/boost_test/usage_variants.html
-//#include <boost/test/included/unit_test.hpp> // Stack Overflow
-/* [lecture 8]
- * BOOST_WARN / BOOST_CHECK / BOOST_REQUIRE     | slide 15
- * _BITWISE_EQUAL / _EQUAL / _SMALL / _CLOSE    | slide 16
- * _NO_THROW / _THROW / _EXCEPTION              | slide 17
- * _MESSAGE / BOOST_ERROR / BOOST_FAIL          | slide 18
- */
-
 /**
  * @brief Holds our colour schemes.
  */
